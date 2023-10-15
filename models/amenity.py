6781e47568e07f3ec2f66f5178045da06b2d3cf3
Twice <njabulo.magagula@ribccs.com>
2023-10-15 15:28:07 +0200

    initial commit

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Amenity class that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """ Class that defines attributes for amenity """
+
+    name = ""
